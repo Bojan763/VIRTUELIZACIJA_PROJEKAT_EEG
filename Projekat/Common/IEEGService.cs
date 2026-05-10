53fe3e1cce5fc2509b13e23863b5c872b62f6953
HP <HP@DESKTOP-LTSDL4R>
2026-05-10 23:39:39 +0200

    Dodavanje IEEGService interfejsa

---
+++ Projekat/Common/IEEGService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [ServiceContract]
+    public interface IEEGService
+    {
+        [OperationContract]
+        AckResponse StartSession(EegMeta meta);
+        [OperationContract]
+        AckResponse PushSample(EegMeta meta);
+        [OperationContract]
+        AckResponse EndSession();
+    }
+}
