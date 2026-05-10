ca86fea8cc45eca54ca66be1360ffe4002bff899
HP <HP@DESKTOP-LTSDL4R>
2026-05-11 00:06:58 +0200

    Hostovanje

---
+++ Projekat/Service/EEGService.cs
@@ -0,0 +1,27 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class EEGService : IEEGService
+    {
+        public AckResponse EndSession()
+        {
+            throw new NotImplementedException();
+        }
+
+        public AckResponse PushSample(EegMeta meta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AckResponse StartSession(EegMeta meta)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Projekat/Service/Program.cs
+++ Projekat/Service/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
+using Common;
 
 namespace Service
 {
@@ -10,7 +12,13 @@ namespace Service
     {
         static void Main(string[] args)
         {
+            ServiceHost host = new ServiceHost(typeof(EEGService));
+            host.Open();
+            Console.WriteLine("EEG Service started, press any key to close it.");
+            Console.ReadKey();
 
+            host.Close();
+            Console.WriteLine("EEG Service is closed");
         }
     }
 }
