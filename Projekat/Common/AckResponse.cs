be99efeda5d8d0eabbc3cd8163cf7ef45905df88
HP <HP@DESKTOP-LTSDL4R>
2026-05-10 23:22:19 +0200

    Dodavanje klasa u Common

---
+++ Projekat/Common/AckResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [DataContract]
+    public class AckResponse
+    {
+        [DataMember]
+        public bool Success { get; set; }
+        [DataMember] 
+        public string Message {  get; set; }
+        [DataMember]
+        public string Status { get; set; }
+    }
+}
---
+++ Projekat/Common/EegMeta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [DataContract]
+    public class EegMeta
+    {
+        [DataMember]
+        public string ParticipantId { get; set; }
+        [DataMember]
+        public string FileName { get; set; }
+        [DataMember]
+        public int TotalRows { get; set; }
+        
+        [DataMember]
+        public string SchemaVersion { get; set; }
+    }
+}
